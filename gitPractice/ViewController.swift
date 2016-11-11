a457f0a9a85ba3d0bd6f9ff73322cc1cf7ca660e
JRJorillo <johnjorillo10@gmail.com>
2016-11-11 22:16:41 +0800

    Initial Commit

---
+++ gitPractice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gitPractice
+//
+//  Created by John Roque deLeon Jorillo on 11/11/2016.
+//  Copyright © 2016 John Roque deLeon Jorillo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
